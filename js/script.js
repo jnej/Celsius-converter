1932fb43b264cf122e524e80d37f489940e4a262
Amanda Johnson <amandajnej@gmail.com>
2017-05-25 21:36:40 +0100

    Add initial code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var converter = function (celsius) {return (celsius * number1) +30};
+
+var celsius = parseInt(prompt("Enter temperature in oC"));
+
+var number1 = 1.8
+
+var result = converter(celsius);
+alert (result);
